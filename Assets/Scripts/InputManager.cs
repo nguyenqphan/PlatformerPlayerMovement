9e02069faa6d60aa18f20fa20caa6be4f4a5fb85
Nguyen Phan <nqphan212@gmail.com>
2015-09-22 14:46:18 -0400

    track button release states

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -17,40 +17,39 @@ public class InputAxisState{
 	public float offValue;
 	public Buttons button;
 	public Condition condition;
-		
+	
 	public bool value{
-
+		
 		get{
 			var val = Input.GetAxis(axisName);
-
+			
 			switch(condition){
 			case Condition.GreaterThan:
 				return val > offValue;
-			case Condition.LessThan: 
+			case Condition.LessThan:
 				return val < offValue;
 			}
-
+			
 			return false;
 		}
+		
 	}
 }
 
 public class InputManager : MonoBehaviour {
-
+	
 	public InputAxisState[] inputs;
 	public InputState inputState;
-
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		foreach(var input in inputs){
-			if(input.value)
-				inputState.SetButtonValue(input.button, input.value);
-				//Debug.Log ("Input Detected "+input.button);
+		foreach (var input in inputs) {
+			inputState.SetButtonValue(input.button, input.value);
 		}
 	}
 }
