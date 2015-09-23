566482103fa97a7775f17e958f5cd6aa7aaed170
Nguyen Phan <nqphan212@gmail.com>
2015-09-23 15:31:25 -0400

    Add a jump effect

--- Assets/Scripts/Behaviors/Jump.cs
+++ Assets/Scripts/Behaviors/Jump.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class Jump : AbstractBehavior {
 
 	public float jumpSpeed = 200f;
+	public float jumpDelay = 0.1f;
+	public int jumCount = 2;
+	public GameObject dustEffectPrefab;
+
+	protected float lastJumpTime = 0;
+	protected int jumpsRemaining = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -15,15 +21,27 @@ public class Jump : AbstractBehavior {
 		var canJump = inputState.GetButtonValue (inputButtons[0]);
 		var holdTime = inputState.GetButtonHoldTime (inputButtons[0]);
 
-		if(collisionState.standing){
-			if(canJump && holdTime < .1f){
-				OnJump();
+		if (collisionState.standing) {
+			if (canJump && holdTime < .1f) {
+				jumpsRemaining = jumCount - 1;
+				OnJump ();
+			}
+		} else {
+			if(canJump && holdTime < 0.1 && Time.time - lastJumpTime > jumpDelay){
+
+				if(jumpsRemaining > 0){
+					OnJump();
+					jumpsRemaining --;
+					var clone = Instantiate(dustEffectPrefab);
+					clone.transform.position = transform.position;
+				}
 			}
 		}
 	}
 
 	protected virtual void OnJump(){
 		var vel = body2d.velocity;
+		lastJumpTime = Time.time;
 		body2d.velocity = new Vector2 (vel.x, jumpSpeed);
 	}
 }
