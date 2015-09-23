2b8b8801714a0ef69a2da3ceff4f943812667554
Nguyen Phan <nqphan212@gmail.com>
2015-09-22 21:58:02 -0400

    Add walking script to the player

---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walk : AbstractBehavior {
+
+	public float speed = 50; //the speed of the player
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var right = inputState.GetButtonValue (inputButtons[0]);
+		var left = inputState.GetButtonValue (inputButtons[1]);
+
+		if(right || left){
+			var tmpSpeed = speed;
+			var velX = tmpSpeed * (float)inputState.direction;
+			body2d.velocity = new Vector2(velX, body2d.velocity.y);
+		}
+	}
+}
