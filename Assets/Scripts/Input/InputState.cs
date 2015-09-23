8825ef0525aa2a56668547ea21273c67234316e9
Nguyen Phan <nqphan212@gmail.com>
2015-09-22 22:28:32 -0400

    Manage animation states

--- Assets/Scripts/Input/InputState.cs
+++ Assets/Scripts/Input/InputState.cs
@@ -15,9 +15,22 @@ public enum Direction{
 public class InputState : MonoBehaviour {
 
 	public Direction direction = Direction.Right;
+	public float absVelX = 0f;
+	public float absVelY = 0f;
 
+
+	private Rigidbody2D body2d;
 	private Dictionary<Buttons, ButtonState> buttonStates = new Dictionary<Buttons, ButtonState>();
-	
+
+	void Awake(){
+		body2d = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate(){
+		absVelX = Mathf.Abs (body2d.velocity.x);
+		absVelY = Mathf.Abs (body2d.velocity.y);
+	}
+
 	public void SetButtonValue(Buttons key, bool value){
 		if(!buttonStates.ContainsKey(key))
 			buttonStates.Add(key, new ButtonState());
