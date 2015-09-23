7feb1d967aec46c2a4f44a6a962d6fb2f739bc1a
Nguyen Phan <nqphan212@gmail.com>
2015-09-22 21:26:46 -0400

    Change the direction the player is facing

---
+++ Assets/Scripts/FaceDirection.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceDirection : AbstractBehavior {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var right = inputState.GetButtonValue (inputButtons[0]);
+		var left = inputState.GetButtonValue (inputButtons[1]);
+	
+		if(right){
+			Debug.Log("Facing Right");
+		}
+		else if(left){
+			Debug.Log("Facing Left");
+		}
+	}
+}
