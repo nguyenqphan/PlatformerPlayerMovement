039db6163b8621befb7f06be6fa7d1355c3d0f0e
Nguyen Phan <nqphan212@gmail.com>
2015-09-22 22:11:04 -0400

    make the player run.

--- Assets/Scripts/Behaviors/Walk.cs
+++ Assets/Scripts/Behaviors/Walk.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Walk : AbstractBehavior {
 
 	public float speed = 50; //the speed of the player
+	public float runMultiplier = 2f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +15,15 @@ public class Walk : AbstractBehavior {
 	void Update () {
 		var right = inputState.GetButtonValue (inputButtons[0]);
 		var left = inputState.GetButtonValue (inputButtons[1]);
+		var run = inputState.GetButtonValue (inputButtons[2]);
 
 		if(right || left){
 			var tmpSpeed = speed;
+
+			if(run && runMultiplier > 0){
+				tmpSpeed *= runMultiplier;
+			}
+
 			var velX = tmpSpeed * (float)inputState.direction;
 			body2d.velocity = new Vector2(velX, body2d.velocity.y);
 		}
